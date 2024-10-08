e0c0a1aa40fe164923b2916b289d48743014c36b
appbels <app.bels@gmail.com>
2024-10-08 17:20:49 +0200

    feat: add custom keymaps for json format

--- init.lua
+++ init.lua
@@ -205,6 +205,11 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+-- [[ Custom Keymaps ]]
+
+-- JSON Format
+vim.keymap.set('n', '<leader>jf', '<cmd>:%!jq .<CR>', { desc = '[J]SON [F]ormat' })
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
